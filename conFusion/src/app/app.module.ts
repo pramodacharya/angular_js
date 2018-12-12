15da35d710572ee1b4b3d822acb8a48942fc100f
Pramod Acharya <pramoddotacharaya@gmail.com>
2018-12-12 00:26:28 -0500

    Angular Components 2

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -21,7 +24,10 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- conFusion/src/app/menu/menu.component.ts
+++ conFusion/src/app/menu/menu.component.ts
@@ -1,28 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import{Dish} from '../shared/dish';
 
-@Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
-})
-export class MenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-dishes: Dish[] = [
-    {
-      id: '0',
-     name: 'Uthappizza',
-     image: '/assets/images/uthappizza.png',
-     category:'mains',
-     featured: true,
-     label: 'Hot',
-     price: '4.99',
-     description: 'A unique comibination of Indian Uthappam (pancake) and Italizn pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Buntur chillies and Buffalo Paneer.'
-    },
+ const DISHES: Dish[] = [
     {
       id: '0',
       name: 'Uthappizza',
@@ -33,6 +12,16 @@ dishes: Dish[] = [
       price: '4.99',
       // tslint:disable-next-line:max-line-length
       description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+    },
+    {
+      id: '1',
+      name: 'Zucchipakoda',
+      image: '/assets/images/zucchipakoda.png',
+      category: 'appetizer',
+      featured: false,
+      label: '',
+      price: '1.99',
+      description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
     },
     {
       id: '2',
@@ -55,4 +44,18 @@ dishes: Dish[] = [
       description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
     }
   ];
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  dishes = DISHES;
+  selectedDish = this.dishes[0];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
