639171c15d18a0766e4b651bd157103dab8519c0
Pramod Acharya <pramoddotacharaya@gmail.com>
2018-12-29 17:43:39 -0500

    Angular Promise more

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms'; 
@@ -63,6 +64,7 @@ import { LoginComponent } from './login/login.component';
     MatCheckboxModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatProgressSpinnerModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule
