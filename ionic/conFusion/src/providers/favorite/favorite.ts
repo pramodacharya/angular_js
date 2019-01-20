687791be8efcd737c35bcd9d927bb89f5b458f53
Pramod Acharya <pramoddotacharaya@gmail.com>
2019-01-20 00:14:06 -0500

    FAB Advanced features

--- ionic/conFusion/src/providers/favorite/favorite.ts
+++ ionic/conFusion/src/providers/favorite/favorite.ts
@@ -1,5 +1,9 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { Dish } from '../../shared/dish';
+import { DishProvider } from '../dish/dish';
 
 /*
   Generated class for the FavoriteProvider provider.
@@ -12,18 +16,38 @@ export class FavoriteProvider {
 
   favorites: Array<any>;
 
-  constructor(public http: Http) {
+  constructor(public http: Http,
+    private dishservice: DishProvider) {
     console.log('Hello FavoriteProvider Provider');
     this.favorites = [];
   }
 
   addFavorite(id: string): boolean {
-    this.favorites.push(id);
+    
+    if (!this.isFavorite(id))        
+      this.favorites.push(id);
     return true;
   }
 
   isFavorite(id: string): boolean {
         return this.favorites.some(el => el === id);
   }
+  
+  getFavorites(): Observable<Dish[]> {
+    return this.dishservice.getDishes()
+      .map(dishes => dishes.filter(dish => this.favorites.some(el => el === dish.id)));
+  }
 
+  deleteFavorite(id: number): Observable<Dish[]> {
+    let index = this.favorites.indexOf(id);
+    if (index >= 0) {
+      this.favorites.splice(index,1);
+      return this.getFavorites();
+    }
+    else {
+      console.log('Deleting non-existant favorite', id);
+      return Observable.throw('Deleting non-existant favorite' + id);
+    }
+  }
+  
 }
